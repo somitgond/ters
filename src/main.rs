924c78ecfe599fa7c879cb763893c790af149347
SomitG <somitgond2@gmail.com>
2026-08-04 21:43:00 +0530

    clear screen

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,55 @@
-use std::{io, os::fd::AsRawFd};
 use std::io::Read;
+use std::{io, os::fd::AsRawFd};
 // use std::os::unix::io::RawFd;
+use std::io::{Write, stdout};
 use termios::*;
-use std::io::{stdout, Write};
 
 // enable terminal raw mode
 fn enable_raw_mode() -> Result<Termios, std::io::Error> {
-  let stdin = io::stdin().as_raw_fd();
-  let mut termios = Termios::from_fd(stdin)?;
-  let termios_orig = termios.clone();
+    let stdin = io::stdin().as_raw_fd();
+    let mut termios = Termios::from_fd(stdin)?;
+    let termios_orig = termios.clone();
 
-  termios.c_cflag |= CS8;
-  // termios.c_cflag &= !(CSIZE | PARENB);
-  termios.c_lflag &= !(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
-  // termios.c_lflag &= !(ECHO | ICANON | ISIG | IEXTEN);
-  termios.c_oflag &= !OPOST;
-  //termios.c_iflag &= !(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
-  termios.c_iflag &= !(BRKINT | ISTRIP | INLCR | ICRNL | IXON);
+    termios.c_cflag |= CS8;
+    termios.c_lflag &= !(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+    termios.c_oflag &= !OPOST;
+    termios.c_iflag &= !(BRKINT | ISTRIP | INLCR | ICRNL | IXON);
 
-  termios.c_cc[VMIN] = 0;
-  termios.c_cc[VTIME] = 1;
-  let _ = tcsetattr(stdin, TCSAFLUSH, &mut termios);
+    termios.c_cc[VMIN] = 0;
+    termios.c_cc[VTIME] = 1;
+    let _ = tcsetattr(stdin, TCSAFLUSH, &mut termios);
 
-  Ok(termios_orig)
+    Ok(termios_orig)
 }
 
 fn disable_raw_mode(mut termios_orig: &Termios) {
-  let stdin = io::stdin().as_raw_fd();
-  let _ = tcsetattr(stdin, TCSAFLUSH, &mut termios_orig);
+    let stdin = io::stdin().as_raw_fd();
+    let _ = tcsetattr(stdin, TCSAFLUSH, &mut termios_orig);
+}
+
+fn write(s: &str) {
+    let mut lock = stdout().lock();
+    write!(lock, "{}", s).unwrap();
+    stdout().flush().unwrap();
+}
+
+fn clear_screen() {
+    write("\x1b[2J");
 }
 
 // main function
 fn main() {
     let mut termios_orig = enable_raw_mode().unwrap();
 
+    clear_screen();
+
     'outer_loop: loop {
-        // read chars 1 byte at a time
+        // read bytes from stdin
         for i in io::stdin().bytes() {
             let c: char = i.unwrap() as char;
-            // println!("Printing: {}", c);
-            // print!("{}", c);
-            let mut lock = stdout().lock();
-            write!(lock, "{}", c).unwrap();
-            stdout().flush().unwrap();
+
+            write(&c.to_string());
+
             if c == 'q' {
                 break 'outer_loop;
             }
