afdd9a547eb0932d131560c805c3ec9b455bfd6f
SomitG <somitgond2@gmail.com>
2026-08-05 23:05:23 +0530

    Cursor movement

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ struct EditorState {
     termios_orig : Termios,
     rows: i32,
     cols: i32,
+    buffer_text: String,
+    cx: i32,
+    cy: i32,
 }
 
 impl EditorState {
@@ -40,8 +43,10 @@ impl EditorState {
         let mut cols: i32 = 0;
 
         loop {
+            // read 1 byte at a time
             let mut buf = [0;1];
             let _ = stdin().read_exact(&mut buf).unwrap();
+
             if buf[0] == 82 {
                 break;
             } else if buf[0] == 59 {
@@ -65,7 +70,9 @@ impl Drop for EditorState {
     fn drop(&mut self) {
         // clear screen and position cursor at top
         clear_screen();
-        reposition_cursor();
+        self.cx = 0;
+        self.cy = 0;
+        reposition_cursor(self);
         self.disable_raw_mode();
     }
 }
@@ -80,31 +87,74 @@ fn clear_screen() {
     write("\x1b[2J");
 }
 
-fn reposition_cursor() {
-    write("\x1b[H");
+fn reposition_cursor(global_state: &EditorState) {
+
+    let mut s = String::from("\x1b[");
+    s.push_str(&(global_state.cy +1).to_string());
+    s.push_str(";");
+    s.push_str(&(global_state.cx +1).to_string());
+    s.push_str("H");
+
+    write(&s);
 }
 
-fn draw_rows(global_state: &EditorState) {
+fn draw_rows(global_state: &mut EditorState) {
+
     for i in 0..global_state.rows {
-        write("~\r\n");
+
+        // show editor version at startup
+        if i == global_state.rows/2 {
+            let s = String::from("Text Editor in RuSt -- version 0.1");
+            // center the welcome message
+            let slen: i32 = s.len() as i32;
+            global_state.buffer_text.push('~');
+            if slen < global_state.cols {
+                for _ in 0..(global_state.cols - slen)/2-1 {
+                    global_state.buffer_text.push(' ');
+                }
+            }
+            global_state.buffer_text.push_str(&s); // erase a line at a time
+        } else {
+            global_state.buffer_text.push('~');
+        }
+
+        global_state.buffer_text.push_str("\x1b[K"); // erase a line at a time
+
+        // tilde on last line also
+        if i < global_state.rows-1 {
+            global_state.buffer_text.push_str("\r\n");
+        }
     }
+    write(&global_state.buffer_text);
 }
 
-fn run_editor(global_state: &EditorState) {
+fn hide_cursor() {
+    write("\x1b[?25l");
+}
+
+fn show_cursor() {
+    write("\x1b[?25h");
+}
+
+fn run_editor(global_state: &mut EditorState) {
     clear_screen();
-    reposition_cursor();
+    reposition_cursor(global_state);
+    hide_cursor();
     draw_rows(global_state);
-    reposition_cursor();
+    reposition_cursor(global_state);
+    show_cursor();
 
     'outer_loop: loop {
         for i in stdin().bytes() {
             let c: char = i.unwrap() as char;
 
-            write(&c.to_string());
-
-            if c == 'q' {
-                break 'outer_loop;
+            match c {
+                'A' => global_state.cx+=1,
+                'E' => global_state.cy+=1,
+                'q' => break 'outer_loop,
+                _ => write(&c.to_string()),
             }
+            reposition_cursor(global_state);
         }
     }
 }
@@ -118,6 +168,9 @@ fn main() {
         termios_orig : Termios::from_fd(stdin_fd).unwrap(),
         rows: 0,
         cols: 0,
+        buffer_text: String::new(),
+        cx: 0,
+        cy: 0,
     };
 
     // enable raw mode
